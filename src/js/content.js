6f47b72131b7da243b81c956a72690cb453c0465
@zip <zip@keybase.io>
2015-08-14 17:51:40 -0700

    Here we get a bit overengineered

--- src/js/content.js
+++ src/js/content.js
@@ -7,8 +7,7 @@
     console.log("Loading");
   }
 
-  let rootNode = document.querySelector(".stream > .stream-items");
-  let selector = ".js-actionable-tweet";
+  let rootNode = "AppContent";
 
   let plugins = {
     _plugins: new Set(),
@@ -26,50 +25,79 @@
   let rot13 = require("./rot13");
   plugins.register(rot13);
 
+  let mappings = {};
+  let componentRegistry = new WeakMap();
+
   let Observers = require("./Observers");
 
-  let destructorObserver = Observers.addRemoveObserver(undefined, (node) => {
+  function augmentClass(node) {
+    if (!node) return;
+    if (node.nodeType != Node.ELEMENT_NODE) return;
+    for (let name of Object.keys(mappings)) {
+      if (node.classList.contains(name)) {
+        return mappings[name].of(node);
+      }
+    }
+  }
+
+  let observer = Observers.addRemoveObserver(augmentClass, (node) => {
     let component = componentRegistry.get(node);
+    for (let name of Object.keys(mappings)) {
+      if (node.nodeType != Node.ELEMENT_NODE) return;
+      for (let subnode of node.querySelectorAll(`.${name}`).iterator) {
+        if (componentRegistry.has(subnode)) {
+          componentRegistry.get(subnode).cleanup();
+        }
+      }
+    }
     if (component && component.cleanup) {
       component.cleanup();
     }
-  });
-  destructorObserver.observe(document.body, {subtree: true});
-
-  let componentRegistry = new WeakMap();
+  }, augmentClass);
+  observer.observe(document.body, {subtree: true});
 
   class Component {
     constructor(element) {
-      this.__element = new WeakSet();
       componentRegistry.set(element, this);
       this.element = element;
-    }
-
-    get element() {
-      for (let element of this.__element) {
-        return element;
+      element.dataset.zipAugment = this.constructor.name;
+      /* Hunt down any nodes to register */
+      for (let name of Object.keys(mappings)) {
+        for (let node of element.querySelectorAll(`.${name}`).iterator) {
+          mappings[name].of(node);
+        }
       }
     }
 
-    set element(value) {
-      this.__element.add(value);
-      return value;
-    }
-
     cleanup() {
-      console.log("Someone deleted me!", this);
+      delete this.element.dataset.zipAugment;
+      componentRegistry.delete(this.element);
     }
   }
 
   class Tweet extends Component {
     constructor(element) {
       super(element);
-      this.element.dataset.zipAugment = "true";
       this.buttons = [];
       plugins.attach(this);
     }
 
+    cleanup() {
+      for (let {name, fn} of this.buttons) {
+        let node = this.element.querySelector(".dropdown-menu > ul > " + `[data-nav="zip-${name}"]`);
+        if (node) node.remove();
+      }
+    }
+
     addButton(name, onClick) {
+      let existing = this.element.querySelector(".js-more-profileTweet-actions .dropdown-menu > ul > " + `[data-nav="zip-${name}"]`)
+      if (existing) {
+        console.log(`${name} already attached`, existing);
+        existing.removeEventListener("click", onClick);
+        existing.addEventListener("click", onClick);
+        return;
+      }
+
       this.buttons.push({name, onClick});
       let li = document.createElement("li");
       let button = document.createElement("button");
@@ -94,6 +122,17 @@
     }
   }
 
-  let s = new Stream(document.querySelector(".stream"));
+  class StreamItem extends Component {
+    static of(element) {
+      return componentRegistry.get(element) || new StreamItem(element);
+    }
+  }
 
+  mappings = {
+    "AppContent": Stream,
+    "js-stream-item": StreamItem,
+    "js-actionable-tweet": Tweet
+  };
+
+  Stream.of(document.querySelector(`.${rootNode}`));
 }).call(this);
